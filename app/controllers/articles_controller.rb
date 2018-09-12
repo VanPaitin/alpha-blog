93764c1f5f1b5f5e84f1ee0858521ea00cdbfb5c
Mayowa Pitan <mayowa.pitan@andela.com>
2018-09-12 16:47:16 +0100

    create show actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def show
-
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -10,8 +10,10 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
+      flash[:notice] = "Article was successfully created"
       redirect_to @article
     else
+      flash[:danger] = "Some errors prevented the article from being saved, please try again"
       render :new
     end
   end
