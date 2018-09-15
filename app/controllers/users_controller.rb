2b17ba47e009a9d2c8ff30650f3de5d78f65d7e3
Mayowa Pitan <mayowa.pitan@andela.com>
2018-09-15 23:32:08 +0100

    finish the sessions create and destroy functionality and update nav bars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -7,23 +9,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:success] = "Your account was updated successfully"
-      redirect_to articles_path
-    else
-      render 'edit'
-    end
   end
 
   def create
@@ -36,9 +23,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
